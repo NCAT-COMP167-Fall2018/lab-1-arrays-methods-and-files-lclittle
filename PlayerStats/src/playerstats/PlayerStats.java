e2326159624cfc8da96abe1d0be64d2412a7c6e0
LaNyia Little <lanyialittle@gmail.com>
2018-08-20 16:54:10 -0400

    While loop runs successfully

--- PlayerStats/src/playerstats/PlayerStats.java
+++ PlayerStats/src/playerstats/PlayerStats.java
@@ -5,17 +5,51 @@
  */
 package playerstats;
 
+import java.io.File;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author lanyi
  */
 public class PlayerStats {
+    
+    public static void readNamesandScores(String[] names, int[] scores, String fileName ){
+        try {
+            //perform some operation that could cause an exception
+            Scanner reader = new Scanner(new File(fileName));
+            int count=0;
+            
+            while(reader.hasNext()){
+                String[] dataLine =reader.nextLine().split(" ");
+                names[count]=dataLine[0];
+                scores[count]=Integer.parseInt(dataLine[1].trim());
+                count++;
+            
+            }
+            
+           // for(int i=0; i<79; i++){
+             //   names[i]=reader.next();
+               // scores[i]=reader.nextInt();
+            //}
+        }
+    
+        catch ( java.io.FileNotFoundException exception ) {
+            //Actions to perform if there is an exception
+            Logger.getLogger(PlayerStats.class.getName()).log(Level.SEVERE, null, exception);
+        } 
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String[] names = new String[80];
+        int[] scores = new int[80];
+        String fileName="playerData2.txt";
     }
     
 }
